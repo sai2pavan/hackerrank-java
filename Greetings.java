95e3a535a1e44fbd5f0a0161ce7a6e5172a635bb
Pavan <loyola.pavan.74@gmail.com>
2025-07-10 20:26:48 +0530

    "#1 challenge"

--- Greetings.java
+++ Greetings.java
@@ -1,5 +1,4 @@
 /*Imagine a software system that welcomes new users as soon as they register or log in for the first time. The system automatically greets the user by displaying "Hello, World!" as soon as the application starts, without waiting for any input. The greeting message helps make the system feel welcoming and interactive.
-
 In this scenario, the system is designed to immediately greet users with the message "Hello, World!" when they access the software for the first time. This behaviour mimics a system that performs an action (like sending a welcome email or showing a message) right after a user logs in. There’s no need for any input from the user; the system automatically prints the greeting when launched.
 
 Sample Output Hello, World!
